cddbb6904d62f221e2f6d84c1d58701de7ee7ce3
Dragos B <bucur.dragos99@gmail.com>
2022-11-01 23:21:32 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -28,18 +28,18 @@ const [ref3, inView3] = useInView({
     <div>
       <Head>
         <title>Create Next App</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="h-screen w-full bg-red-400 overflow-scroll snap snap-y snap-mandatory overflow-x-hidden xl:overflow-auto xl:snap-none xl:flex xl:w-screen xl:h-screen xl:justify-center xl:items-center xl:px-10">
+      <div className="h-screen w-full overflow-scroll snap snap-y snap-mandatory overflow-x-hidden xl:overflow-auto xl:snap-none xl:flex xl:w-screen xl:h-screen xl:justify-center xl:items-center xl:px-10">
         <Burger />
-        <div className='my-4 xl:h-full xl:flex-1 xl:my-0' ref={ref1}>
+        <div className='xl:h-full xl:flex-1 xl:my-0' ref={ref1}>
           <Card />
         </div>
         <div className='my-4 xl:h-full xl:flex-1 xl:my-0' ref={ref2}>
           <Card />
         </div>
-        <div className='my-4 xl:h-full xl:flex-1 xl:my-0' ref={ref3}>
+        <div className='xl:h-full xl:flex-1 xl:my-0' ref={ref3}>
           <Card />
         </div>
         <Dots bgColor1={inView1 ? 'bg-white': ''} bgColor2={inView2 ? 'bg-white': ''} bgColor3={inView3 ? 'bg-white': ''} />
