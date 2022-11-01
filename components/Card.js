1d392d1e050051c8e8bbbdec29e26e5c715eaad4
Dragos B <bucur.dragos99@gmail.com>
2022-11-01 22:43:29 +0200

    Update Card.js

--- components/Card.js
+++ components/Card.js
@@ -2,7 +2,7 @@ export default function Card() {
 
     return (
         <div className="relative h-screen w-full py-20 snap-start px-3 xl:h-full xl:snap-none xl:px-5">
-            <div className="bg-cardColor h-full w-full rounded-lg xl:shadow-md" />
+            <div className="bg-red-400 h-full w-full rounded-lg xl:shadow-md" />
         </div>
     )
 }
