6c383e1fb7fc722565261bdcadd3dd5c3c6d5ce9
Dragos B <bucur.dragos99@gmail.com>
2022-11-02 19:23:32 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className='relative flex flex-col h-screen'>
+      <div className='relative flex flex-col h-screen lg:w-full'>
         <Burger />
         <CardsLayout />
       </div>
