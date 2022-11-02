d21edccd829ed55d761f987ccc42d0075935d520
Dragos B <bucur.dragos99@gmail.com>
2022-11-02 12:32:21 +0200

    Update CardsLayout.js

--- components/CardsLayout.js
+++ components/CardsLayout.js
@@ -1,10 +1,10 @@
 export default function CardsLayout(props) { 
 
     return (
-        <div className="snap-mandatory snap-y gap-20 px-3 py-20 flex flex-col h-screen w-full mx-auto overflow-scroll"> 
-            <div className="snap-center snap-always shrink-0 h-full w-full rounded-lg xl:shadow-md xl:h-full" style={{backgroundColor: props.color}} />
-            <div className="snap-center snap-always shrink-0 h-full w-full rounded-lg xl:shadow-md xl:h-full" style={{backgroundColor: props.color}} />
-            <div className="snap-center snap-always shrink-0 h-full w-full rounded-lg xl:shadow-md xl:h-full" style={{backgroundColor: props.color}} />
+        <div className="snap-mandatory snap-y gap-20 px-5 py-20 flex flex-col h-screen w-full mx-auto overflow-scroll"> 
+            <div className="snap-center snap-always shrink-0 w-full rounded-lg xl:shadow-md xl:h-full" style={{backgroundColor: props.color, height: '90%'}} />
+            <div className="snap-center snap-always shrink-0 w-full rounded-lg xl:shadow-md xl:h-full" style={{backgroundColor: props.color, height: '90%'}} />
+            <div className="snap-center snap-always shrink-0 w-full rounded-lg xl:shadow-md xl:h-full" style={{backgroundColor: props.color, height: '90%'}} />
         </div>
     )
 }
