d3f7cc7505bd4bf815b968de5c8b7283330bb470
Dragos B <bucur.dragos99@gmail.com>
2022-11-02 12:07:54 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,37 +2,19 @@ import Head from 'next/head'
 import Card from '../components/Card'
 import Dots from '../components/Dots'
 import Burger from '../components/Burger'
-import { useInView } from 'react-intersection-observer'
+import { useState, useRef, useEffect } from "react"
 
 export default function Home() {
-
-const [ref1, inView1] = useInView({
-  /* Optional options */
-  rootMargin: '0px 0px',
-  unobserveOnEnter: true
-})
-
-const [ref2, inView2] = useInView({
-  /* Optional options */
-  rootMargin: '0px 0px',
-  unobserveOnEnter: true
-})
-
-const [ref3, inView3] = useInView({
-  /* Optional options */
-  rootMargin: '0px 0px',
-  unobserveOnEnter: true
-})
 
   return (
     <div>
       <Head>
         <title>Create Next App</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
-        <Burger />
+      <Burger />
+      <div className='snap-mandatory snap-y flex flex-col h-screen w-full mx-auto overflow-scroll'>
         <Card />
         <Card />
         <Card />
