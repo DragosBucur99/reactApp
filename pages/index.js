207c94712cf77dcb24de73aabfac3920f59cb611
Dragos B <bucur.dragos99@gmail.com>
2022-11-02 17:51:37 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
 import Head from 'next/head'
 import CardsLayout from '../components/CardsLayout'
-import Dots from '../components/Dots'
 import Burger from '../components/Burger'
-import { useState, useRef, useEffect } from "react"
 
 export default function Home() {
 
   return (
-    <div>
+    <div className='h-full'>
       <Head>
         <title>Create Next App</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
