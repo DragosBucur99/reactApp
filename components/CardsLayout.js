133406a459a0132df847e690fd6f065b2600beab
Dragos B <bucur.dragos99@gmail.com>
2022-11-02 19:23:16 +0200

    Update CardsLayout.js

--- components/CardsLayout.js
+++ components/CardsLayout.js
@@ -2,10 +2,13 @@
 export default function CardsLayout(props) { 
 
     return (
-        <div className="flex-1 relative snap-mandatory snap-y px-4 flex flex-col h-1/2 w-full mx-auto overflow-scroll overflow-y-scroll gap-10">
-            <div className="snap-start snap-always shrink-0 w-full rounded-lg h-full xl:shadow-md xl:h-full" style={{backgroundColor: props.color}} />
-            <div className="snap-start snap-always shrink-0 w-full rounded-lg h-full xl:shadow-md xl:h-full" style={{backgroundColor: props.color}} />
-            <div className="snap-start snap-always shrink-0 w-full rounded-lg h-full xl:shadow-md xl:h-full" style={{backgroundColor: props.color}} />
+        <div className="flex-1 relative snap-mandatory snap-y px-4 pb-4 flex flex-col h-1/2 w-full mx-auto overflow-scroll overflow-y-scroll gap-10 lg:flex-row lg:overflow-hidden lg:py-20 lg:px-20 md:px-20">
+            <div className="flex flex-col gap-5 snap-start snap-always shrink-0 w-full rounded-lg h-full lg:shadow-md lg:h-full lg:flex-1">
+                <div className="w-full h-[30%] bg-red-400 rounded-lg" style={{backgroundColor: props.color}}></div>
+                <div className="w-full flex-1 bg-red-400 rounded-lg" style={{backgroundColor: props.color}}></div>
+            </div>
+            <div className="snap-start snap-always shrink-0 w-full rounded-lg h-full lg:shadow-md lg:h-full lg:flex-1" style={{backgroundColor: props.color}} />
+            <div className="snap-start snap-always shrink-0 w-full rounded-lg h-full lg:shadow-md lg:h-full lg:flex-1" style={{backgroundColor: props.color}} />
         </div>
     )
 }
