79caac1eb3200dae220d408c460da143f960b314
Dragos B <bucur.dragos99@gmail.com>
2022-11-16 21:00:37 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Head from 'next/head'
 import CardsLayout from '../components/CardsLayout'
 import Burger from '../components/Burger'
+import {motion} from "framer-motion"
+
 
 export default function Home() {
 
   return (
-    <div className='h-full'>
+    <motion.div className='h-full' exit={{ opacity: 0 }}>
       <Head>
         <title>Create Next App</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -15,6 +17,6 @@ export default function Home() {
         <Burger />
         <CardsLayout />
       </div>
-    </div>
+    </motion.div>
   )
 }
